4e6d157e92fecfedac5911c5a003dfeaa51e1148
Heshbon <cityalight.hesbon@gmail.com>
2023-09-07 15:35:03 -0700

    4puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - my first c program code
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
