4ea64be0c111e5fccd599e302a5e37d287b96416
Heshbon <cityalight.hesbon@gmail.com>
2023-09-10 04:40:35 -0700

    Prints all possible combinations of single digits numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combinations of single digit numbers
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+		putchar(i);
+	putchar(',');
+		putchar(' ');
+	putchar('\n');
+	return (0);
+}
