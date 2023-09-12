be2e7a2baf2016ed14b5e659ff063862d581f33c
Heshbon <cityalight.hesbon@gmail.com>
2023-09-12 05:46:03 -0700

    2-print_alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10  - Prints ten times the alphabet
+ *
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int b;
+	char j;
+
+	for (b = 0 ; b < 10 ; b++)
+	{
+		for (j = 'a' ; j <= 'z' ;  j++)
+			_putchar(j);
+	}
+	_putchar('\n');
+}
