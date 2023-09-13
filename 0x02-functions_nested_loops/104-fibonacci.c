a3b30822bf25f8ac3f6bec8f3613832519dd0958
Heshbon <cityalight.hesbon@gmail.com>
2023-09-13 06:29:14 -0700

    finds and prints the first 98 Fibonacci numbers, starting with 1 and 2

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -12,8 +12,6 @@ int main(void)
 	unsigned long int aft = 2;
 	unsigned long int l = 1000000000;
 	unsigned long int fib1;
-	unsigned long int fib2;
-	unsigned long int aft1;
 	unsigned long int aft2;
 
 	printf("%lu", fib);
@@ -26,18 +24,14 @@ int main(void)
 	}
 
 	fib1 = (fib / l);
-	fib2 = (fib % l);
-	aft1 = (aft / l);
-	aft2 = (aft % l);
+	fib1 = (fib % l);
 
 	for (a = 93; a < 99; ++a)
 	{
 		printf(", %lu", fib1 + (aft2 / l));
 		printf("%lu", aft2 % l);
-		fib1 = aft1 + fib1;
-		fib1 = aft1 - fib1;
-		aft2 = aft2 + fib2;
-		fib2 = aft2 - fib2;
+		fib1 = aft2 + fib1;
+	        fib1 = aft2 - fib1;
 	}
 	printf("\n");
 	return (0);
