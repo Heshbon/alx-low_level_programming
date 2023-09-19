bcec1f136890d95f3f3c39e23c6541b25e67512a
Heshbon <cityalight.hesbon@gmail.com>
2023-09-19 00:59:24 -0700

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,13 +10,13 @@ void print_array(int *a, int n)
 {
 	int b;
 
-	for (b = 0 ; b < n ; b++)
+	for (b = 0; b < (n - 1); b++)
 	{
 		printf("%d", a[b]);
-		if (b != (n - 1))
+		if (b == (n - 1))
 		{
-			printf(", ");
+			printf("%d", a[n - 1]);
 		}
-		printf("\n");
+			printf("\n");
 	}
 }
