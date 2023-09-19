c59d2e66a4926bd82ebb0833f5efb526dc103486
Heshbon <cityalight.hesbon@gmail.com>
2023-09-19 02:45:43 -0700

    Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - a program that prints the numbers from 1 to 100
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 1; a <= 100; a++)
+	{
+	if (a % 3 == 0 && a % 5 != 0)
+	{
+	printf(" Fizz");
+	}
+	else if (a % 5 == 0 && a % 3 != 0)
+	{
+	printf(" Buzz");
+	}
+	else if (a % 3 == 0 && a % 5 == 0)
+	{
+	printf(" FizzBuzz");
+	}
+	else if (a == 1)
+	{
+	printf("%d", a);
+	}
+	else
+	{
+	printf(" %d", a);
+	}
+	}
+	printf("\n");
+	return (0);
+}
