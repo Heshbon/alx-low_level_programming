8ce8abc8b33ff89eb93d117bb28170dd4cc1e4ba
Heshbon <cityalight.hesbon@gmail.com>
2023-09-21 11:49:09 -0700

    main.h

---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd>
+
+/**
+ * _putchar - checks the character
+ * @c: the character to print
+ *
+ * Return: 1 when success and -1 is returned
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
