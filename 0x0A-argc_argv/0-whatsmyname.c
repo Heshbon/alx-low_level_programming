5159990aba2a84d9ab7b25face4cccfda086a721
Heshbon <cityalight.hesbon@gmail.com>
2023-09-29 08:47:43 -0700

    a program that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - a program that prints its name
+ * @argc: counts the arguement being passed
+ * @argv: the array of characters
+ * Return: 0
+ */
+
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
