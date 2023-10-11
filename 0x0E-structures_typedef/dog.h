4fb0b30c9171405dad8b94229d1906409ecdf32a
Heshbon <cityalight.hesbon@gmail.com>
2023-10-10 23:44:18 -0700

    struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - the structure to check
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the name of the owner
+ * Return: 0
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
