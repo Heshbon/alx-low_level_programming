480641920455caa7dd7b55556f862d217d331bed
Heshbon <cityalight.hesbon@gmail.com>
2023-10-24 04:51:19 -0700

    function that returns the number of elements in a linked listint_t list

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,6 +2,7 @@
 
 /**
  * listint_len - function that returns the number of elements
+ * in a linked listint_t list
  * @h: the head of the linked list
  * Return: the number of elements in a linked listint_t list
  */
