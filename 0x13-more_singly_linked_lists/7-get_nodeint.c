f70b8b866fc1332e4b60328e7158320d47547777
Heshbon <cityalight.hesbon@gmail.com>
2023-10-24 16:41:27 -0700

    function that returns the sum of all the data (n) of a listint_t linked list

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - function that returns the
+ * nth node of a listint_t linked list
+ * @head: the pointer to the head
+ * @index: the index of the node starting at 0
+ * Return: nth node or NULL if failed
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *p;
+	unsigned int a;
+
+	p = head;
+	a = 0;
+	while (a < index)
+	{
+		if (p)
+			p = p->next;
+		else
+			return (NULL);
+		a++;
+	}
+	if (p)
+		return (p);
+	else
+		return (NULL);
+}
