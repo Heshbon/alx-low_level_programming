e0f700f8aef3e78d368b38607efd6ed40f72ffe8
Heshbon <cityalight.hesbon@gmail.com>
2023-10-30 07:47:17 -0700

    function that reads a text file and prints it to the POSIX stdout

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+#include <fcntl.h>
+
+/**
+ * read_textfile - function that reads a text file
+ * and prints it to the POSIX stdout
+ * @filename: the character string
+ * @letters: the number of letters to read and print
+ * Return: the actual number of letters it could read and print
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t n, p;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		free(buffer);
+		return (0);
+	}
+	n = read(fd, buffer, letters);
+	if (n == -1)
+		return (0);
+
+	p = write(STDOUT_FILENO, buffer, n);
+	if (p == -1 || n != p)
+		return (0);
+	free(buffer);
+
+	close(fd);
+	return (p);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,5 @@
+0x15. C - File I/O
+
+
+
+#Task assignment on File I/O
