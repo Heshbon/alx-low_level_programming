eb2805b2127417297a7600f19954687f51d42df7
Heshbon <cityalight.hesbon@gmail.com>
2023-12-22 12:52:36 -0800

    hash_tables.h

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -84,8 +84,8 @@ typedef struct shash_table_s
 
 shash_table_t *shash_table_create(unsigned long int size);
 int shash_table_set(shash_table_t *ht, const char *key, const char *value);
-void add_shash_node(shash_table_t *ht, shash_node_t *node);
-void add_shash_order(shash_table_t *ht, shash_node_t *node);
+void create_shash_node(shash_table_t *ht, shash_node_t *node);
+void create_shash_order(shash_table_t *ht, shash_node_t *node);
 char *shash_table_get(const shash_table_t *ht, const char *key);
 void shash_table_print(const shash_table_t *ht);
 void shash_table_print_rev(const shash_table_t *ht);
