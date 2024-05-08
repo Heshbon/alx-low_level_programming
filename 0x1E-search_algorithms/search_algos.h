9a46154e2a594871047c2367b20b08b1672c6164
Heshbon <cityalight.hesbon@gmail.com>
2024-05-08 07:32:49 -0700

    search_algos.h

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,28 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+/**
+ * struct listint_s - singly linked list
+ *
+ * @p: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct listint_s
+{
+	int p;
+	size_t index;
+	struct listint_s *next;
+} listint_t;
+
+/* Prototypes */
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif /* SEARCH_ALGOS_H */
